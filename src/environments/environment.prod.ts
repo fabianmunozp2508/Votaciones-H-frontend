c4020eda2cbe57260d60a8f3979a9a85dec47a85
Fabian Jhon Muñoz Puello <78973548+fabianmunozp2508@users.noreply.github.com>
2022-03-15 18:09:47 -0500

    Delete src/environments directory

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  base_urlFace: 'http://localhost:8080/vph/auth/facebook/token',
-  base_url: 'http://localhost:8080/vph',
-  base_urlogin:'http://localhost:8080/vph/login'
-};
